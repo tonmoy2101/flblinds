cf969854c292aab5cd98644a1d40313306d438e9
tonmoy2101 <62930710+tonmoy2101@users.noreply.github.com>
2022-11-24 00:41:23 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!-- Site Footer Starts -->
+
+<footer class="site-footer">
+        <div class="footer-row">
+
+            <div class="footer-col">
+                <h4>PRODUCTS</h4>
+                <ul class="footer-widget">
+                    <li><a href="./orlando-venetian-blinds.html">Horizontal Blinds / Venetian Blinds</a></li>
+                    <li><a href="./orlando-s-shape-verical-blinds.html">Vertical Blinds</a></li>
+                    <li><a href="./orlando-shutters.html">Shutters</a></li>
+                    <li><a href="./orlando-roller-shades.html">Roller shades & Sliding panels</a></li>
+                    <li><a href="./orlando-valances.html">Valances</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h4>SUPPORT</h4>
+                <ul class="footer-widget">
+                    <li><a href="./about-us.html">About Us</a></li>
+                    <li><a href="./contact.html">Contact Us</a></li>
+                    <li><a href="./faq.html">FAQ</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h4>GET IN TOUCH</h4>
+                <ul>
+                    <li class="footer-contact-into">Call Us : <span><a href="tel:4079698300">407.969.8300</a></span>
+                    </li>
+                    <li class="footer-contact-into">Mail : <span><a
+                                href="mailto:info@flblinds.com">info@flblinds.com</a></span></li>
+                </ul>
+                <ul class="social-links">
+                    <li><a href="https://www.facebook.com/floridablindsllc" target="_blank"><i
+                                class="fa-brands fa-facebook social-icon-footer"></i></a></li>
+                    <li><a href="https://twitter.com/FLBLINDS"><i class="fa-brands fa-twitter social-icon-footer"
+                                target="_blank"></i></a></li>
+                    <li><a href="https://www.instagram.com/flblinds/"><i
+                                class="fa-brands fa-instagram social-icon-footer" target="_blank"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/florida-blinds-llc"><i
+                                class="fa-brands fa-linkedin social-icon-footer" target="_blank"></i></a></li>
+                </ul>
+            </div>
+
+        </div>
+    </footer>
+    <div class="copyright">
+        <p class="text-center">© Copyright 2022 | <span><a href="./index.html">Flblinds</a></span> | All Rights
+            Reserved.</p>
+    </div>
+
+    <!-- Site Footer Ends  -->
+
+    <script>
+        const accordion = document.getElementsByClassName('contentBx');
+
+        for (let i = 0; i < accordion.length; i++) {
+            accordion[i].addEventListener('click', function () {
+                this.classList.toggle('active')
+            })
+        }
+    </script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Orlando's Leading Blinds Company - Florida Blinds</title>
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://kit.fontawesome.com/e49dbe749c.js" crossorigin="anonymous" type="text/javascript"></script>
+
+    <!-- Google Font  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Site Header Starts  -->
+
+    <header class="site-header">
+
+        <a href="./index.html" class="logo"><img class="logo-img" src="./img/home/logo.png" alt=""></a>
+
+        <input type="checkbox" id="menu-bar">
+        <label for="menu-bar">
+            <i class="fa-solid fa-bars"></i>
+        </label>
+
+        <nav class="navbar">
+            <ul class="navbar-lists">
+                <li class="nav-single-list"><a class="nav-single-list-link" href="./index.html">HOME</a></li>
+                <li class="nav-single-list"><a class="nav-single-list-link" href="./about-us.html">ABOUT US</a></li>
+                <li class="nav-single-list"><a class="nav-single-list-link" href="./products.html">PRODUCT</a>
+                    <ul class="navbar-sub">
+                        <li class="navbar-sub-single"><a class="navbar-sub-single-link"
+                                href="./orlando-venetian-blinds.html">HORIZONTAL BLINDS / VENETIAN BLINDS</a></li>
+                        <li class="navbar-sub-single"><a class="navbar-sub-single-link"
+                                href="./orlando-s-shape-verical-blinds.html">VERTICAL BLINDS</a></li>
+                        <li class="navbar-sub-single"><a class="navbar-sub-single-link"
+                                href="./commercial-blind-experts.html">COMMERCIAL BLINDS</a></li>
+                        <li class="navbar-sub-single"><a class="navbar-sub-single-link"
+                                href="./orlando-shutters.html">SHUTTERS</a></li>
+                        <li class="navbar-sub-single"><a class="navbar-sub-single-link"
+                                href="./orlando-roller-shades.html">REMOTE CONTROL ROLLER SHADES</a></li>
+                        <li class="navbar-sub-single"><a class="navbar-sub-single-link"
+                                href="./cellular-shades.html">CELL SHADES</a></li>
+                        <li class="navbar-sub-single"><a class="navbar-sub-single-link"
+                                href="./orlando-valances.html">VALANCES</a></li>
+                    </ul>
+                </li>
+                <li class="nav-single-list"><a class="nav-single-list-link" href="./faq.html">FAQ</a></li>
+                <li class="nav-single-list"><a class="nav-single-list-link" href="./contact.html">CONTACT</a></li>
+                <li class="nav-single-list"><a class="nav-single-list-link" href="./testimonials.html">TESTIMONIAL</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Site Header Ends  -->
